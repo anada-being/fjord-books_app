4951243cbfb6cba6914966559206fb5c6c2d2bf4
anada <anada@beingcorp.co.jp>
2024-01-29 13:13:14 +0900

    deviseの初期設定（configとapp）とカラムの追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   resources :books
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
-  root to: "users#sign_in"
+  root to: "books#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -24,6 +24,9 @@ ActiveRecord::Schema.define(version: 2024_01_25_082633) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "postal_code"
+    t.string "address"
+    t.text "profile"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
