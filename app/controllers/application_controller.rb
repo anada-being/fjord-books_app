12b4ef250a797748f7f0c49e424708730601a63f
anada <anada@beingcorp.co.jp>
2024-01-29 18:40:13 +0900

    ログインしていないと本の一覧が見れないように

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,9 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:account_update, keys:[:postal_code, :address, :profile])
+  end
 end
