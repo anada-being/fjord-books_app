5cb1462aa088bfda434c35b50d5a77daf6b482c1
anada <anada@beingcorp.co.jp>
2024-01-30 09:22:30 +0900

    rubocopとユーザー一覧画面をログイン中でないと見れないようにした

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,6 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:account_update, keys:[:postal_code, :address, :profile])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[postal_code address profile])
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
+# frozen_string_literal: true
+
 class UsersController < ApplicationController
+  before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def list
@@ -7,7 +10,6 @@ class UsersController < ApplicationController
   end
 
   def user_params
-    params.require(:user).permit(:show, keys:[:email, :postal_code, :address, :profile])
+    params.require(:user).permit(:show, keys: %i[email postal_code address profile])
   end
-
 end
