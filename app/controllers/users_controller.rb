f81057351c4cae1a845f41b090761d5b164a316f
anada <anada@beingcorp.co.jp>
2024-02-05 16:02:36 +0900

    listからindexに変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,12 +3,16 @@
 class UsersController < ApplicationController
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  def list
+  def index
     @users = User.order(:id).page(params[:page])
-    @users = @users.page(params[:page])
+    @users = @users.page
+  end
+
+  def show
+    @user = User.find(params[:id])
   end
 
   def user_params
-    params.require(:user).permit(:show, keys: %i[email postal_code address profile])
+    params.require(:user).permit(keys: %i[email postal_code address profile])
   end
 end
