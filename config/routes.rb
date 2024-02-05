3a79b0b5f4e796980cfc8e108b45dab5b01396c1
anada <anada@beingcorp.co.jp>
2024-02-05 16:13:58 +0900

    onlyで最低限のpathに変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :books, :users
+  resources :books
+  resources :users, only:[:index, :show]
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   root to: "books#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
