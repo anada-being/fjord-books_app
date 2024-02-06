f13b39015ff3aacd1c74c4711ec948095d6c8407
anada <anada@beingcorp.co.jp>
2024-02-06 09:53:05 +0900

    ActiveRecordモデルで翻訳を行う準備（上手く行っていない）

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  scope '(:locale)', locale: /#{I18n.available_locales.map(&:to_s).join('|')}/ do
-    resources :books
-    # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  end
+  resources :books
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
