463293d68130cb8eb0d179ae877cc86c4f95a7b7
anada <anada@beingcorp.co.jp>
2024-03-04 14:07:46 +0900

    rubocop実施。空白の削除

--- app/controllers/followers_controller.rb
+++ app/controllers/followers_controller.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class FollowersController  < ApplicationController
+class FollowersController < ApplicationController
   def show
     @title = FollowRelationship.human_attribute_name(:followers)
     @user = User.find(params[:user_id])
