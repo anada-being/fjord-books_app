ce0d6d9c3b833e2e30f8b779347ec6940332dad3
anada <anada@beingcorp.co.jp>
2024-03-04 11:28:42 +0900

    referをやめ、詳細画面にredirectさせる

--- app/controllers/follow_relationships_controller.rb
+++ app/controllers/follow_relationships_controller.rb
@@ -4,12 +4,12 @@ class FollowRelationshipsController < ApplicationController
   def create
     user = User.find(params[:followed_id])
     current_user.follow(user)
-    redirect_to request.referer
+    redirect_to user_path(user.id)
   end
 
   def destroy
     user = User.find(params[:followed_id])
     current_user.unfollow(user)
-    redirect_to request.referer
+    redirect_to user_path(user.id)
   end
 end
