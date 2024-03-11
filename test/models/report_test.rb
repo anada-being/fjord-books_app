6cee2d4cbfe1ab100a6ae4f2bcdf15613779c432
anada <anada@beingcorp.co.jp>
2024-03-11 14:53:26 +0900

    レポートモデルのモデルテスト

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -3,7 +3,26 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'CRUD_report' do
+    me = User.create!(email: 'me@example.com', password: 'password')
+    she = User.create!(email: 'she@example.com', password: 'password')
+    nippo = me.reports.new(title: 'today', content: 'TT')
+    assert_nil Report.find_by(title: 'today', content: 'TT')
+
+    assert nippo.save
+    assert Report.find_by(title: 'today', content: 'TT')
+    nippo_id = nippo.id
+    assert nippo.editable?(me)
+    assert_not Report.find(nippo_id).editable?(she)
+
+    assert nippo.update(title: 'aaa', content: 'omg')
+    assert_equal 'aaa', Report.find(nippo_id).title
+    assert nippo.editable?(me)
+    assert_not Report.find(nippo_id).editable?(she)
+
+    assert_equal Date.today, nippo.created_on
+
+    nippo.destroy
+    assert_nil Report.find_by(id: nippo_id)
+  end
 end
